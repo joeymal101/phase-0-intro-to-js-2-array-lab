e1fa3e9507db45d09a25e38fa3b7623cf4f10deb
Joell Malone <joellmalone000@gmail.com>
2023-07-06 11:46:22 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat(name) {
+  cats.push('Ralph');
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift('Bob');
+}
+function destructivelyRemoveLastCat() {
+   cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+const newCats = 'Broom';
+function appendCat(name) {
+  return [...cats, 'Broom'];
+}
+const newCat = 'Arnold';
+function prependCat(name) {
+  return ['Arnold', ...cats];
+}
+function removeLastCat() {
+   return cats.slice(0, -1);
+}
+function removeFirstCat() {
+  return cats.slice(1);
+}
